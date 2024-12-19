e1858f7d8d7e23d2b7ba83ef96da345839b7df60
shahboz <sharafzodashahboz@gmail.com>
2024-12-19 16:16:31 +0500

    GUI app removed

---
+++ ConsoleApp1/Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1;
+public class Calculator
+{
+    public int Add(int number1, int number2)
+    {
+        return number1 + number2;
+    }
+
+    public int Subtract(int number1, int number2)
+    {
+        return number1 - number2;
+    }
+
+    public int Multiply(int number1, int number2)
+    {
+        return number1 * number2;
+    }
+
+    public double Divide(int number1, int number2)
+    {
+        if (number2 == 0) //Hande division by zero
+        {
+            Console.WriteLine("Error: Division by zero");
+        }
+
+        return (double)number1 / number2;
+    }
+    
+}
