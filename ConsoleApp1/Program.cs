bad8a984991802572b074c785d911116972149b3
shahboz <sharafzodashahboz@gmail.com>
2024-12-19 09:17:01 +0500

    project added

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp1;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        const int number = 5;
+        var s = number.ToString();
+        Console.WriteLine($"The value of the number is:{number}");
+        Console.WriteLine($"The type of the number is:{s.GetType()}");
+
+        var digit = 2;
+        var count = 10;
+        var totalPrice = 20.95f;
+        var firstName = "Tommy";
+        var isWorking = false;
+
+        Console.WriteLine(digit);
+        Console.WriteLine(count);
+        Console.WriteLine(totalPrice);
+        Console.WriteLine(firstName);
+        Console.WriteLine(isWorking);
+        
+
+
+        #region Checking
+
+        var checker = new Checker();
+        checker.Check();
+
+        #endregion
+    }
+}
---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace GUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSubmitClick(object sender, RoutedEventArgs e)
+        {
+            string name = NameInput.Text; // Get the name input
+            GreetingText.Text = $"Hello {name}!"; // Update the TextBlock
+        }
+    }
+}
